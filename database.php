ab153572a8b1b334d51c8716d9f6c5b2ca2108a8
Alexander-Araeb <153088102+Alexander-Araeb@users.noreply.github.com>
2024-04-08 14:46:34 +0200

    Update database.php

--- database.php
+++ database.php
@@ -11,4 +11,17 @@ if ($mysqli->connect_errno) {
     die("Connection error: ". $mysqli->connect_error);
 }
 
+
+<?php
+$dbhost = 'blueaxis-demo.cho86uycqgm7.eu-north-1.rds.amazonaws.com';
+$dbport = '3306';
+$dbname = 'blueaxis-demo';
+$charset = 'utf8' ;
+
+$dsn = "mysql:host={$dbhost};port={$dbport}";
+$username = 'dread';
+$password = 'dread9643!';
+
+$mysqli = new PDO($dsn, $username, $password);
+
 return $mysqli;
